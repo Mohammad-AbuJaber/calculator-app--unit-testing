e957cc2c7e9ef14a98f739e6dfb6db37b9e72caa
Mohammad-AbuJaber <mohammadabujaber2001@gmail.com>
2023-10-01 14:20:11 +0300

    Order of Operations and Multiple Operators

--- calculator.test.js
+++ calculator.test.js
@@ -50,4 +50,31 @@ describe('Calculator', () => {
             expect(result).toBe(2);
         });
     });
+    describe('Order of Operations and Multiple Operators', () => {
+        it('should follow the correct order of operations', () => {
+            const operands = [2, '+', 3, '*', 4];
+            const result = calc(...operands);
+            expect(result).toBe(14);
+        });
+
+        it('should throw an error for an invalid operator', () => {
+            const operands = [5, '$', 3];
+            expect(() => calc(...operands)).toThrow('Invalid operator');
+        });
+
+        it('should throw an error for invalid input types', () => {
+            const operands = ['2', '+', 3];
+            expect(() => calc(...operands)).toThrow('Invalid input type');
+        });
+
+        it('should throw an error for multiple operators in a row', () => {
+            const operands = [5, '+', '*', 3, '/'];
+            expect(() => calc(...operands)).toThrow('Invalid input');
+        });
+
+        it('throws error for invalid input', () => {
+            const operands = ['+', 5, '+', 3, 3];
+            expect(() => calc(...operands)).toThrow('Invalid input');
+        });
+    });
 });
